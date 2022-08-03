eeb4ee29c5f4c0b13749eb39610b118cadd05158
Nazar Gorynych <nazarii.horchytsia@oa.edu.ua>
2022-08-03 16:42:58 +0300

    model tables created

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from receipts.models import Receipt, User
+
 
 # Create your models here.
+
+class OrderingPreference(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    receipts = models.ManyToManyField(Receipt)
--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Receipt, User
+
 
 # Register your models here.
+class Admin(UserAdmin):
+    model = User
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(Receipt)
