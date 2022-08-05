c7ff22c184029cf368daa4fa3184d10e1bfdaa77
Nazar Gorynych <nazarii.horchytsia@oa.edu.ua>
2022-08-05 16:54:05 +0300

    index url redirect for login, fix login error-message

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, IndexView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('', IndexView.as_view(), name='index'),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserCreationForm
+
+
+class IndexView(TemplateView, LoginRequiredMixin):
+    template_name = 'receipts_home/index.html'
 
 
 class SignUpView(CreateView):
