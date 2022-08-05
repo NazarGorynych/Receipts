474bc4c45724d394075b523de2efb44f617eafe2
Nazar Gorynych <nazarii.horchytsia@oa.edu.ua>
2022-08-05 16:11:31 +0300

    error messages on login/register, DRY refactor receipts.forms

--- receipts/views.py
+++ receipts/views.py
@@ -2,11 +2,10 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-from .forms import UserRegisterForm
+from .forms import UserCreationForm
 
 
-class SignUpView(CreateView, SuccessMessageMixin):
-    form_class = UserRegisterForm
-    success_url = reverse_lazy('signup')
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
     template_name = 'registration/sign-up.html'
-    success_message = 'Success!'
