f6e44fc696a06f810fe874fe5170c7453c02209b
Nazar Gorynych <nazarii.horchytsia@oa.edu.ua>
2022-08-06 18:42:49 +0300

    OrderingPreference autocreation on registration, and update on new Receipt creation

--- preferences/models.py
+++ preferences/models.py
@@ -6,4 +6,13 @@ from receipts.models import Receipt, User
 
 class OrderingPreference(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    receipts = models.ManyToManyField(Receipt)
+    receipts = models.ManyToManyField(Receipt, blank=True)
+
+    def save(self, *args, **kwargs):
+        super(OrderingPreference, self).save(*args, **kwargs)
+
+        # autoincrement ManyToManyRel with Receipts on new Receipts
+        # and on initial user Registration,
+        # declared after super(), since pk is needed for ManyToMany
+        receipts = Receipt.objects.all()
+        self.receipts.add(*receipts)
--- receipts/views.py
+++ receipts/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserCreationForm
+from preferences.models import OrderingPreference
 
 
 class IndexView(TemplateView, LoginRequiredMixin):
@@ -13,3 +14,8 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/sign-up.html'
+
+    def get_success_url(self):
+        instance = OrderingPreference(user_id=self.object.id)
+        instance.save()
+        return super(SignUpView, self).get_success_url()
