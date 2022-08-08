2217e814ed4912d7c60c36be6c3ec86e0c679981
Nazar Gorynych <nazarii.horchytsia@oa.edu.ua>
2022-08-08 23:27:48 +0300

    frontend sorting linked with db to change order of receipts on drag&drop

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import SignUpView, IndexView
+from .views import SignUpView, IndexView, sort
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('', IndexView.as_view(), name='index'),
 ]
+
+htmx_urlpatterns = [
+    path('sort/', sort, name='sort'),
+]
+
+urlpatterns += htmx_urlpatterns
--- receipts/views.py
+++ receipts/views.py
@@ -1,22 +1,40 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView, TemplateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserCreationForm
 from preferences.models import OrderingPreference
 from .models import Receipt, User
 from .middleware import get_current_user
+from django.shortcuts import render
 
 
 class IndexView(TemplateView, LoginRequiredMixin):
     template_name = 'receipts_home/index.html'
 
     def get_context_data(self, **kwargs):
-        context_data = super(IndexView, self).get_context_data()
-        context_data['users'] = User.objects.all()
         preference = OrderingPreference.objects.get(user=get_current_user())
+        context_data = super(IndexView, self).get_context_data()
+
+        context_data['users'] = User.objects.all()
+        context_data['preference'] = preference
         context_data['receipts'] = preference.retrieve_receipts_by_id()
         return context_data
+
+def sort(request):
+    '''
+     Reassigns ordered by SortableJS receipts to OrderingPreference,
+     and swaps form where it was sorted with the new one to continue the cycle
+     '''
+
+    ordered_receipts = request.POST.getlist('receipt_order')
+    preference = OrderingPreference.objects.get(user_id=get_current_user())
+    preference.receipts = ordered_receipts
+    preference.save()
+    receipts = preference.retrieve_receipts_by_id()
+    return render(request, 'receipts_home/includes/receipts.html', {'receipts': receipts})
 
 
 class SignUpView(CreateView):
